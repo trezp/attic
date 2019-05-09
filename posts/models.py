a78b36fe609f0aa3357b6a3a84edf40c10494c29
Treasure <treasure.rose@gmail.com>
2019-05-09 00:22:58 -0700

    add posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, default="This is a title")
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, ListView
+from .models import Post
 # Create your views here.
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
 
 
 class AboutPageView(TemplateView):
