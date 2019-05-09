f531de835eead4a7359ee259827704e269ec5d66
Treasure <treasure.rose@gmail.com>
2019-05-09 01:10:18 -0700

    basic blog

--- posts/models.py
+++ posts/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=50, default="This is a title")
-    text = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.title
